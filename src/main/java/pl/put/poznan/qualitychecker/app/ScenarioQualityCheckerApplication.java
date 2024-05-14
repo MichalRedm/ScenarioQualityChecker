3a56802fdbdc31117b93657a3dc1bf17dd335d71
MichalRedm <45372892+MichalRedm@users.noreply.github.com>
2024-05-14 13:16:49 +0200

    Remove possibly problematic whitespace characters

--- src/main/java/pl/put/poznan/qualitychecker/app/ScenarioQualityCheckerApplication.java
+++ src/main/java/pl/put/poznan/qualitychecker/app/ScenarioQualityCheckerApplication.java
@@ -11,23 +11,23 @@ public class ScenarioQualityCheckerApplication {
     public static void main(String[] args) {
         SpringApplication.run(ScenarioQualityCheckerApplication.class, args);
         Scenario scenario = new Scenario("""
-                                            Title: Book addition
-                                            Actors: Librarian,TestGuy
-                                            System actor: System
+Title: Book addition
+Actors: Librarian,TestGuy
+System actor: System
 
-                                            * Librarian selects options to add a new book item
-                                            * A form is displayed.
-                                            * Librarian provides the details of the book.
-                                            * IF: Librarian wishes to add copies of the book
-                                              - Librarian chooses to define instances
-                                              - System presents defined instances
-                                              - FOR EACH: instance:
-                                                * Librarian chooses to add an instance
-                                                * System prompts to enter the instance details
-                                                * Librarian enters the instance details and confirms them.
-                                                * System informs about the correct addition of an instance and presents the updated list of instances.
-                                            * Librarian confirms book addition.
-                                            * System informs about the correct addition of the book."""
+* Librarian selects options to add a new book item
+* A form is displayed.
+* Librarian provides the details of the book.
+* IF: Librarian wishes to add copies of the book
+  - Librarian chooses to define instances
+  - System presents defined instances
+  - FOR EACH: instance:
+    * Librarian chooses to add an instance
+    * System prompts to enter the instance details
+    * Librarian enters the instance details and confirms them.
+    * System informs about the correct addition of an instance and presents the updated list of instances.
+* Librarian confirms book addition.
+* System informs about the correct addition of the book."""
                 );
         System.out.println(scenario.toJSON());
     }
