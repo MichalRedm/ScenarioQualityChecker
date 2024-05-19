960601baad80b5926fe23c02c539dc3c40555f11
MichalRedm <45372892+MichalRedm@users.noreply.github.com>
2024-05-19 21:40:45 +0200

    Cleanup

--- src/main/java/pl/put/poznan/qualitychecker/app/ScenarioQualityCheckerApplication.java
+++ src/main/java/pl/put/poznan/qualitychecker/app/ScenarioQualityCheckerApplication.java
@@ -2,7 +2,6 @@ package pl.put.poznan.qualitychecker.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import pl.put.poznan.qualitychecker.logic.Scenario;
 
 /**
  * Main class of the project.
