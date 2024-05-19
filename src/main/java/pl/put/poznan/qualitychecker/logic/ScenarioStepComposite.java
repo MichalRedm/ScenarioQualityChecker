37ebf552344c71d84b3f8eb8078c999ec373444c
MichalRedm <45372892+MichalRedm@users.noreply.github.com>
2024-05-19 12:59:04 +0200

    Javadoc for ScenarioStepComposite

--- src/main/java/pl/put/poznan/qualitychecker/logic/ScenarioStepComposite.java
+++ src/main/java/pl/put/poznan/qualitychecker/logic/ScenarioStepComposite.java
@@ -2,22 +2,38 @@ package pl.put.poznan.qualitychecker.logic;
 
 import java.util.List;
 
+/** Step within a scenario that has substeps. */
 public class ScenarioStepComposite implements ScenarioStepComponent {
 
+    /** Type of the composite step (IF, ELSE, FOR EACH). */
     private ScenarioStepCompositeType type;
+    /** Text within a step. */
     private String text;
+    /** List of substeps of the composite step. */
     private List<ScenarioStepComponent> substeps;
 
+    /**
+     * Creates a new instance of class {@link ScenarioStepComposite}.
+     * @param type Type of the composite step represented by {@link ScenarioStepCompositeType}.
+     * @param text Text within a step.
+     */
     public ScenarioStepComposite(ScenarioStepCompositeType type, String text) {
         this.type = type;
         this.text = text;
         this.substeps = new java.util.ArrayList<>();
     }
 
+    /**
+     * Adds a step to the list of substeps
+     * @param substep Step to be added as a substep.
+     */
     public void addSubstep(ScenarioStepComponent substep) {
         substeps.add(substep);
     }
 
+    /**
+     * @return Type of the composite step (IF, ELSE, FOR EACH).
+     */
     public ScenarioStepCompositeType getType() {
         return type;
     }
@@ -27,6 +43,9 @@ public class ScenarioStepComposite implements ScenarioStepComponent {
         return text;
     }
 
+    /**
+     * @return List of substeps of the composite step.
+     */
     public List<ScenarioStepComponent> getSubsteps() {
         return substeps;
     }
