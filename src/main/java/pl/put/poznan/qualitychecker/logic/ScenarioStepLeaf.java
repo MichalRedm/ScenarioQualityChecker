4fd9be18cb1e91c8e7e25760c2279bf5eb335673
MichalRedm <45372892+MichalRedm@users.noreply.github.com>
2024-05-19 13:02:17 +0200

    JavaDoc for ScenarioStepLeaf

--- src/main/java/pl/put/poznan/qualitychecker/logic/ScenarioStepLeaf.java
+++ src/main/java/pl/put/poznan/qualitychecker/logic/ScenarioStepLeaf.java
@@ -1,9 +1,15 @@
 package pl.put.poznan.qualitychecker.logic;
 
+/** Simple step within a scenario that has no substeps. */
 public class ScenarioStepLeaf implements ScenarioStepComponent {
 
+    /** Text within a step. */
     private String text;
 
+    /**
+     * Creates a new instance of class {@link ScenarioStepLeaf}.
+     * @param text Text within a step.
+     */
     public ScenarioStepLeaf(String text) {
         this.text = text;
     }
