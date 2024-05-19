7d5171a14447d97e13bf8c41fcd07025415751d1
MichalRedm <45372892+MichalRedm@users.noreply.github.com>
2024-05-19 14:40:26 +0200

    Update ScenarioQualityChecker.java

--- src/main/java/pl/put/poznan/qualitychecker/logic/ScenarioQualityChecker.java
+++ src/main/java/pl/put/poznan/qualitychecker/logic/ScenarioQualityChecker.java
@@ -85,7 +85,6 @@ public class ScenarioQualityChecker {
         List<ScenarioStepComponent> invalidSteps = new ArrayList<>();
 
         var actors = scenario.getAllActors();
-        actors.add("System");
         for (var step : scenario.getAllSteps()) {
             boolean contain = false;
             for (String actor : actors) {
