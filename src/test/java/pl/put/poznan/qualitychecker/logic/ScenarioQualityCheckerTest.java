f362a0a7771cdc30fc3a554137f86791b221ad42
addobosz <adamdobosz.poczta@gmail.com>
2024-05-21 17:29:07 +0200

    add class for testing ScenarioQualityChecker

---
+++ src/test/java/pl/put/poznan/qualitychecker/logic/ScenarioQualityCheckerTest.java
@@ -0,0 +1,26 @@
+package pl.put.poznan.qualitychecker.logic;
+
+import org.junit.jupiter.api.Test;
+
+class ScenarioQualityCheckerTest {
+
+    @Test
+    void countAllSteps() {
+    }
+
+    @Test
+    void countConditionalDecisions() {
+    }
+
+    @Test
+    void getInvalidSteps() {
+    }
+
+    @Test
+    void toText() {
+    }
+
+    @Test
+    void simplify() {
+    }
+}
