17ff193c51e66c3f0ad3ca24f0c2809dcc6448be
Wector <>
2024-06-10 07:40:29 +0200

    Fix invalid steps visitor test

--- src/test/java/pl/put/poznan/qualitychecker/logic/ScenarioGetInvalidStepsVisitorTest.java
+++ src/test/java/pl/put/poznan/qualitychecker/logic/ScenarioGetInvalidStepsVisitorTest.java
@@ -12,31 +12,23 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ScenarioGetInvalidStepsVisitorTest {
     @Test
-    public void visitStepLeaf() {
+    public void visitIsTextInvalid() {
         ScenarioStepLeaf stepLeaf = Mockito.mock(ScenarioStepLeaf.class);
+        var visitor = new ScenarioGetInvalidStepsVisitor();
         Mockito.when(stepLeaf.getText())
                 .thenReturn("Librarian selects options to add a new book item")
                 .thenReturn("A form is displayed.")
                 .thenReturn("Librarian provides the details of the book.");
+        ArrayList<String> actors =
+                new ArrayList<>(Arrays.asList("Librarian", "System"));
+        visitor.setActors(actors);
         List<ScenarioStepComponent> invalidSteps = new ArrayList<>();
         for (int i = 0; i < 3; i++) {
-            if (isTextInvalid(stepLeaf.getText())) {
+            if (visitor.isTextInvalid(stepLeaf.getText())) {
                 invalidSteps.add(stepLeaf);
             }
         }
         assertEquals(1, invalidSteps.size());
     }
 
-    private boolean isTextInvalid(String text) {
-        boolean isInvalid = true;
-        ArrayList<String> actors =
-                new ArrayList<>(Arrays.asList("Librarian", "System"));
-        for (String actor : actors) {
-            if (text.startsWith(actor)) {
-                isInvalid = false;
-                break;
-            }
-        }
-        return isInvalid;
-    }
 }
